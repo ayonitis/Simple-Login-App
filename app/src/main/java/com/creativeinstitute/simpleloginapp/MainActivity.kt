aa87bcff092a30992237de16d057012281ab713a
ayonitis <ayon3015@gmail.com>
2024-08-10 12:53:59 +0600

    initial commit

---
+++ app/src/main/java/com/creativeinstitute/simpleloginapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.creativeinstitute.simpleloginapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.math.log
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var usernameInput: EditText
+    lateinit var passwordInput: EditText
+    lateinit var loginBtn: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        usernameInput = findViewById(R.id.text_username)
+        passwordInput = findViewById(R.id.text_password)
+        loginBtn = findViewById(R.id.login_btn)
+
+        loginBtn.setOnClickListener {
+            val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+
+        }
+
+    }
+}
